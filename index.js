6ec3c9a3b1c387fd19cf20196cb050f70dc193ae
Alexey Gordeyev <aleksej@gordejev.lv>
2013-02-21 00:25:16 +0200

    Fix expose version

--- index.js
+++ index.js
@@ -12,19 +12,20 @@ exports.__defineGetter__('BaseSQL', function () {
 });
 
 exports.init = function (rw) {
-    if (typeof rw === 'string') {
-        railway.orm = exports;
+    if (global.railway) {
+        global.railway.orm = exports;
     } else {
         rw.orm = {Schema: exports.Schema, AbstractClass: exports.AbstractClass};
     }
-    require('./lib/railway')(rw);
+    var railway = './lib/railway';
+    require(railway)(rw);
 };
 
 try {
     if (process.versions.node < '0.6') {
         exports.version = JSON.parse(fs.readFileSync(__dirname + '/package.json')).version;
     } else {
-        exports.version = require('../package').version;
+        exports.version = require('./package').version;
     }
 } catch (e) {}
 
