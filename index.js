74923384af7041321b983cece1bd38d1ed2908f9
Alexey Gordeyev <aleksej@gordejev.lv>
2013-02-20 23:39:30 +0200

    Fix nodeunit tests

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ exports.Schema = require('./lib/schema').Schema;
 exports.AbstractClass = require('./lib/abstract-class').AbstractClass;
 exports.Validatable = require('./lib/validatable').Validatable;
 
+var baseSQL = './lib/sql';
+
+exports.__defineGetter__('BaseSQL', function () {
+    return require(baseSQL);
+});
+
 exports.init = function (rw) {
     if (typeof rw === 'string') {
         railway.orm = exports;
@@ -22,3 +28,7 @@ try {
     }
 } catch (e) {}
 
+var commonTest = './tests/common_test';
+exports.__defineGetter__('test', function () {
+    return require(commonTest);
+});
