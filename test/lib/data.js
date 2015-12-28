95b2b863b5674f42a0f4e96f36f3f87ed9c4f8ff
Alexey Gordeyev <aleksej@gordejev.lv>
2015-12-28 13:57:00 +0200

    fix  data.js for tests

---
+++ test/lib/data.js
@@ -0,0 +1,140 @@
+/**
+ * Created by Alex on 12/27/2015.
+ */
+module.exports.categories = [
+    {
+        active: 0,
+        category_id: 0,
+        title: 'My Category 1',
+        section: 'my-category'
+    },
+    {
+        active: 0,
+        category_id: 1,
+        title: 'My Category 2',
+        section: 'my-category-2'
+    },
+    {
+        active: 0,
+        category_id: 1,
+        title: 'My Category 3',
+        section: 'my-category-3'
+    },
+    {
+        active: 0,
+        category_id: 2,
+        title: 'My Category 4',
+        section: 'my-category-4'
+    },
+    {
+        active: 0,
+        category_id: 2,
+        title: 'My Category 5',
+        section: 'my-category-5'
+    },
+    {
+        active: 0,
+        category_id: 2,
+        title: 'My Category 6',
+        section: 'my-category-6'
+    },
+    {
+        active: 0,
+        category_id: 3,
+        title: 'My Category 7',
+        section: 'my-category-7'
+    }
+];
+
+module.exports.articles = [
+    {
+        active: 0,
+        language: 'en',
+        category_id: 0,
+        title: 'My Article 1',
+        alias: 'my-article-1',
+        mainpage: 0,
+        params: {
+            title: 1,
+            categories: 1
+        }
+    },
+    {
+        active: 0,
+        language: 'en',
+        category_id: 1,
+        title: 'My Article 2',
+        alias: 'my-article-2',
+        mainpage: 0,
+        params: {
+            title: 1,
+            categories: 1
+        }
+    },
+    {
+        active: 0,
+        language: 'en',
+        category_id: 1,
+        title: 'My Article 3',
+        alias: 'my-article-3',
+        mainpage: 0,
+        params: {
+            title: 1,
+            categories: 1
+        }
+    },
+    {
+        active: 0,
+        language: 'en',
+        category_id: 1,
+        title: 'My Article-4',
+        alias: 'my-article-4',
+        mainpage: 0,
+        params: {
+            title: 1,
+            categories: 1
+        }
+    },
+    {
+        active: 0,
+        language: 'en',
+        category_id: 1,
+        title: 'My Article-5',
+        alias: 'my-article-5',
+        mainpage: 0,
+        params: {
+            title: 1,
+            categories: 1
+        }
+    }
+];
+
+module.exports.users = [
+    {
+        language: 'en',
+        first_name: 'Alex',
+        last_name: 'Gordan',
+        screen_name: 'alex',
+        email: 'bubles@example.com',
+        password: 'aaaaaaaaaa',
+        age: 45
+    },
+    {
+        language: 'en',
+        first_name: 'Marco',
+        last_name: 'Polo',
+        screen_name: 'polo',
+        email: 'polo@example.com',
+        password: 'xxxxxxxxxx',
+        age: 95
+    },
+    {
+        language: 'en',
+        first_name: 'Nataly',
+        last_name: 'Prier',
+        screen_name: 'nataly',
+        email: 'nataly@example.com',
+        password: 'nnnnnnnnnn',
+        age: 21
+    }
+];
